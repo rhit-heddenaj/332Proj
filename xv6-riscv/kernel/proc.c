102a418d5e034be002cc7dd3582ff35f3aa5ab79
AJ <heddenaj@rose-hulman.edu>
2025-02-09 15:39:15 -0500

    added fork to spoon

--- xv6-riscv/kernel/proc.c
+++ xv6-riscv/kernel/proc.c
@@ -686,7 +686,53 @@ uint64 spoon(void* arg)
 {
   // Add your code here...
   printf("In spoon system call with argument %p\n", arg);
-  return 0;
+  
+  int i, pid;
+  struct proc *np;
+  struct proc *p = myproc();
+
+  // Allocate process.
+  if((np = allocproc()) == 0){
+    return -1;
+  }
+
+  // Copy user memory from parent to child.
+  if(uvmcopy(p->pagetable, np->pagetable, p->sz) < 0){
+    freeproc(np);
+    release(&np->lock);
+    return -1;
+  }
+  np->sz = p->sz;
+
+  // copy saved user registers.
+  *(np->trapframe) = *(p->trapframe);
+
+  // Cause fork to return 0 in the child.
+  np->trapframe->a0 = 0;
+
+  // increment reference counts on open file descriptors.
+  for(i = 0; i < NOFILE; i++)
+    if(p->ofile[i])
+      np->ofile[i] = filedup(p->ofile[i]);
+  np->cwd = idup(p->cwd);
+
+  safestrcpy(np->name, p->name, sizeof(p->name));
+
+  pid = np->pid;
+
+  release(&np->lock);
+
+  acquire(&wait_lock);
+  np->parent = p;
+  release(&wait_lock);
+
+  acquire(&np->lock);
+  np->state = RUNNABLE;
+  release(&np->lock);
+
+  return pid;
+
+
 }
 
 uint64 mythread_create(int arg1, void* arg2) {
